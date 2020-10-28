ef79e3fb0a92d9464a5f3f99a7cf46e55d057cc6
tuoying96 <tuoying96@gmail.com>
2020-10-27 23:42:35 -0400

    Update index.js

--- airbnb/routes/index.js
+++ airbnb/routes/index.js
@@ -4,26 +4,28 @@ var router = express.Router();
 // var sqlite3 = require('sqlite3').verbose();
 const myDB = require("../db/myDB.js");
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
-
-db.close();
-
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.redirect("/hosts");
+});
+
+router.get("/hosts", async (req, res) => {
+  const page = req.query.page || 1;
+  console.log("/hosts", page);
+
+  try {
+    const songs = await myDB.getHosts(page);
+    // console.log("got songs", songs);
+    res.render("hosts", {
+      hosts: hosts,
+      err: req.session.err,
+      msg: req.session.msg,
+    });
+  } catch (err) {
+    console.log("got error", err);
+    res.render("hosts", { err: err.message, songs: [] });
+  }
 });
 
 module.exports = router;
